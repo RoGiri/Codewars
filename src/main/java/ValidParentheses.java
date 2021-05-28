9df0fe397c23cb5d704d133874b816a26929cc7b
Rojan Giri <rojangiri.rg@gmail.com>
2021-05-29 00:39:35 +0100

    Valid Parentheses solved with comments

--- src/main/java/ValidParentheses.java
+++ src/main/java/ValidParentheses.java
@@ -3,22 +3,22 @@ import java.util.Stack;
 public class ValidParentheses {
 
     public static boolean validParentheses(String parens) {
-        Stack<Character> characters = new Stack<>();
-        for (char c : parens.toCharArray()) {
+        Stack<Character> characters = new Stack<>(); // stack to store the the char
+        for (char c : parens.toCharArray()) {// loop though the array
 
             if (c == '(') {
-                characters.push(c);
+                characters.push(c); // push the char to the top of the stack
             }
 
             else if (c == ')' && !characters.isEmpty() && characters.peek() == '(') {
-                characters.pop();
+                characters.pop(); // remove the stack if the char is present int eh array
             }
-            else if(c == ')' && characters.isEmpty() ){
-                return false;
+            else if(c == ')' && characters.isEmpty() ){ // check is the stack is empty
+                return false; // return false
 
             }
             else {
-                continue;
+                continue; // continue to the next char  if the its not the '(' and ')'
             }
 
         }
